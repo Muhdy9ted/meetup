5f7d38768645b70da190c6c09144a1a04a20683e
Muhammed Abdullahi <MABDULLAHI@unionbankng.com>
2022-07-24 07:59:33 +0100

    added firebase backend services to save meetups and used context api for passing data and managing application state throughout the application

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import { FavoritesContextProvider } from './store/favorites-context';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <FavoritesContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </FavoritesContextProvider>,
+  document.getElementById('root')
 );
